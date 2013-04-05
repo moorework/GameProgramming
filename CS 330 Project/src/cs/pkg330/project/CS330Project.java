fb36dc46767d096bc6d8978a2a020aeb9645467c
James Moore <moore.work@live.com>
2013-04-05 09:56:33 -0400

    First Commit to get NB Project online

---
+++ CS 330 Project/src/cs/pkg330/project/CS330Project.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs.pkg330.project;
+
+/**
+ *
+ * @author moore
+ */
+public class CS330Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
