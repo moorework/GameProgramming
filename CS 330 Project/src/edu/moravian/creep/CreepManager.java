e0228a4e2ccb1fc6c12d752388414b67a2227517
Myles Barros <m_barros@outlook.com>
2013-04-11 18:37:44 -0400

    Added CreepManager (interface not implementation).

---
+++ CS 330 Project/src/edu/moravian/creep/CreepManager.java
@@ -0,0 +1,47 @@
+package edu.moravian.creep;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author myles
+ */
+public class CreepManager {
+
+    public CreepManager(ArrayList<Creep> initialCreeps)
+    {
+        // TODO implement me
+    }
+    
+    public CreepManager()
+    {
+        // TODO implement me
+    }
+    
+    public void addCreep(Creep newCreep)
+    {
+        // TODO implement me
+    }
+    
+    public int getNumCreeps()
+    {
+        // TODO implement me
+        return 0;
+    }
+    
+    public Iterable getCreeps()
+    {
+        // TODO implement me
+        return null;
+    }
+    
+    public void update(double delta)
+    {
+        
+    }
+    
+    private void pruneCreeps()
+    {
+        
+    }
+}
