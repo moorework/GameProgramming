b6e03122a5d02ba360049074029ad6222b3d875a
Myles Barros <m_barros@outlook.com>
2013-04-11 17:13:31 -0400

    Fleshed out CreepManager interface.

--- CS 330 Project/src/edu/moravian/creep/CreepManager.java
+++ CS 330 Project/src/edu/moravian/creep/CreepManager.java
@@ -28,9 +28,27 @@ public class CreepManager {
         
     }
     
+    private void pruneCreeps()
+    {
+        
+    }
+    
     public Creep getClosestCreepToPoint(Point2D point)
     {
         // TODO implement me
         return null;
+    }
+    
+    public int creepsBeingManaged()
+    {
+        // TODO implement me
+        return 0;
+    }
+    
+    public Iterable getIterable()
+    {
+        // TODO implement me
+        return null;
+        
     }
 }
