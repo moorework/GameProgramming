872726474fc878c9230a9eeab00af194e7f9fa18
Myles Barros <m_barros@outlook.com>
2013-04-20 21:42:28 -0400

    Path implementation.

--- CS 330 Project/src/edu/moravian/creep/pathfinding/Path.java
+++ CS 330 Project/src/edu/moravian/creep/pathfinding/Path.java
@@ -1,15 +1,35 @@
 package edu.moravian.creep.pathfinding;
 
 import edu.moravian.math.Point2D;
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  *
  * @author myles
  */
 public class Path {
+    private ArrayList<Point2D> waypoints;
     
+    public Path(Collection<Point2D> waypoints) {
+        this.waypoints = (ArrayList) waypoints;
+    }
+    
+    /**
+     * Determines whether the path contains further waypoints.
+     * @return true if there is at least one more waypoint, false otherwise.
+     */
+    public boolean hasNextWayPoint() {
+        return !waypoints.isEmpty();
+    }
+    
+    /**
+     * Retrieve the next waypoint on the Path. Note that each waypoint can only
+     * be retrieved once.
+     * 
+     * @return the next waypoint on the path
+     */
     public Point2D getNextWayPoint() {
-        // TODO implement me
-        return null;
+        return waypoints.remove(0);
     }
 }
