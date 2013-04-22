5d447ab4b985a7dab1cac9015eee0be31e57888a
James Moore <moore.work@live.com>
2013-04-22 10:14:29 -0400

    Beginnings of map appearance

--- CS 330 Project/src/edu/moravian/WorldMap/MapBuilder.java
+++ CS 330 Project/src/edu/moravian/WorldMap/MapBuilder.java
@@ -1,17 +1,25 @@
 package edu.moravian.WorldMap;
 
+import com.sun.imageio.plugins.jpeg.JPEGImageWriter;
+import edu.moravian.Settings;
 import edu.moravian.math.Point2D;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Set;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriter;
 
 /**
  *
  * @author myles
  */
-public class MapBuilder
-  {
+public class MapBuilder {
 
     private static final char PATH_CHAR = 'P';
     private static final char TOWER_CHAR = 'T';
@@ -21,8 +29,19 @@ public class MapBuilder
     private static Point2D cachedStartingPoint;
     private static Point2D cachedEndingPoint;
 
-    protected static ArrayList<ArrayList<WorldCell>> getMapRepresentation(String mapDirLocation) throws FileNotFoundException
-      {
+    
+    public static void main(String[] args) throws FileNotFoundException, IOException{
+        Settings.getInstance().setResolution(new Dimension(800,600));
+        
+        BufferedImage buf = createImageReppresentation(getMapRepresentation("foo"));
+     
+        File outputFile = new File("image.png");
+    ImageIO.write(buf, "PNG", outputFile);
+    }
+            
+    
+    
+    protected static ArrayList<ArrayList<WorldCell>> getMapRepresentation(String mapDirLocation) throws FileNotFoundException {
 
         //TODO test if there is no file
 
@@ -38,51 +57,43 @@ public class MapBuilder
 
         ret.add(translate(first));
 
-        while (s.hasNextLine())
-          {
+        while (s.hasNextLine()) {
             String temp = s.nextLine();
-            if (temp.length() != first.length())
-              {
+            if (temp.length() != first.length()) {
                 throw new IllegalArgumentException("Lines must all contain same number of charactars");
-              }
+            }
 
             ret.add(translate(temp));
 
-          }
+        }
 
         //TODO pull out appearenceID at some point; last line of file?
 
 
         return ret;
-      }
+    }
 
-    protected static int getAppearenceID(String mapDirLocation)
-      {
+    protected static int getAppearenceID(String mapDirLocation) {
 
         return cachedAppearenceID;
-      }
+    }
 
-    protected static Point2D getStartingPoint(String mapDirLocation)
-      {
+    protected static Point2D getStartingPoint(String mapDirLocation) {
 
         return cachedStartingPoint;
-      }
+    }
 
-    protected static Point2D getEndingPoint(String mapDirLocation)
-      {
+    protected static Point2D getEndingPoint(String mapDirLocation) {
 
         return cachedEndingPoint;
-      }
+    }
 
     //TODO test round one 
-    private static ArrayList<WorldCell> translate(String first)
-      {
+    private static ArrayList<WorldCell> translate(String first) {
         //TODO store translation values elsewhere?
         ArrayList<WorldCell> ret = new ArrayList<WorldCell>();
-        for (char c : first.toCharArray())
-          {
-            switch (c)
-              {
+        for (char c : first.toCharArray()) {
+            switch (c) {
                 case PATH_CHAR:
                     ret.add(new PathCell());
                     break;
@@ -97,9 +108,53 @@ public class MapBuilder
                     break;
                 default:
                     throw new IllegalArgumentException("Data file must be composed of legal cells");
-              }
-          }
+            }
+        }
         return ret;
-      }
-  }
+    }
+
+    private static BufferedImage createImageReppresentation(ArrayList<ArrayList<WorldCell>> ret) {
+        Dimension res = Settings.getInstance().getResolution();
+        int blockWidth = res.width / ret.get(0).size();
+        int blockHeight = res.height / ret.get(0).size();
+
+
+        BufferedImage buf = new BufferedImage(res.width, res.height, BufferedImage.TYPE_INT_RGB);
+
+        for (int i = 0; i < ret.size(); i++) {
+            for (int j = 0; j < ret.get(i).size(); j++) {
+
+
+                for (int k = 0; k < blockWidth; k++) {
+                    for (int l = 0; l < blockHeight; l++) {
+                        buf.setRGB(k + i * blockWidth, l + j * blockHeight, translateColor(ret.get(i).get(j)));
+                    }
+                }
+
+
+            }
+        }
+
+        return buf;
+    }
+
+    private static int translateColor(WorldCell t) {
+        if (t instanceof PathCell) {
+            return Color.red.getRed();
+        } else if (t instanceof StartCell) {
+
+
+            return Color.black.getRGB();
+        } else if (t instanceof EndCell) {
+
+            return Color.ORANGE.getRGB();
+        } else if (t instanceof TowerCell) {
+
+            return Color.white.getRGB();
+        } else {
+            return -1;
+
+        }
+    }
+}
 //TODO make a map verifier 
